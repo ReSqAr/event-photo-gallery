783003e8332cab79b45091329881f451d8c7ce0d
Yasin Zähringer <yasinzaehringer@yhjz.de>
2017-06-15 14:21:55 +0100

    pagination

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,4 +1,36 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  results: Ember.computed('model', function() {
+    return this.get('model');
+  }),
+
+  hasNext: Ember.computed('results', function() {
+    return this.get('results.next');
+  }),
+
+  hasPrevious: Ember.computed('results', function() {
+    return this.get('results.previous');
+  }),
+
+  actions: {
+    next: function() {
+      const nextURL = this.get('results.next');
+      if (nextURL) {
+        let that = this;
+        Ember.$.getJSON(nextURL).then(function(ret) {
+          that.set("results",ret);
+        });
+      }
+    },
+    previous: function() {
+      const previousURL = this.get('results.previous');
+      if (previousURL) {
+        let that = this;
+        Ember.$.getJSON(previousURL).then(function(ret) {
+          that.set("results",ret);
+        });
+      }
+    },
+  }
 });
