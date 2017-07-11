f052c164752e33dd133ecb07b797b6574759f689
Yasin Zähringer <yasinzaehringer@yhjz.de>
2017-07-11 01:40:56 +0100

    improved gallery layout & fixed heart button

--- app/components/gallery-item.js
+++ app/components/gallery-item.js
@@ -1,7 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    truncatedComment: Ember.computed('item.comment', function() {
+  classNames: ['col-md-3','col-sm-6', 'col-xs-12'],
+
+  truncatedComment: Ember.computed('item.comment', function() {
         const comment = this.get('item.comment');
         if( comment.length > 70 ) {
             return comment.slice(0,70).trim() + '…';
@@ -13,6 +15,7 @@ export default Ember.Component.extend({
     actions: {
         liked() {
             this.set('item.likes', this.get('item.likes') + 1);
+            event.stopPropagation();
         },
     },
 
